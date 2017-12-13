22ab024e5946989d9f50d463d3d40448a7289d14
Hritik Soni (CW) <Hritik.Soni@jda.com>
2017-12-13 10:45:29 +0530

    Fixed Migration Issue

--- Core/apps.py
+++ Core/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'Core'
+
+	# def ready(self):
+	# 	pass
+class CustomConfig(AppConfig):
+	name ='Core'
+	def ready(self):
+		pass
